9d4b1c750d435da58de64372482a15f03eaf93be
Momoily <9736419+chenzhen1998@user.noreply.gitee.com>
2022-02-16 11:03:07 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-
+    <el-button>默认按钮</el-button>
   </div>
 </template>
 
 <script>
   export default {
-    
+    props:{
+      wuhu:{
+        tyep:String,
+        default:""
+      },
+    },
   }
 </script>
 
